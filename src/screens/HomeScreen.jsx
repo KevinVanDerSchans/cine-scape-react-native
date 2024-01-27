8ad2f54cb2396c9a1c2c38c994ce5aae6403785e
Kevin Schans <kevinvdsd@hotmail.com>
2024-01-27 14:54:26 +0100

    Create TrendingMovies structure

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,27 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ScrollView } from 'react-native-web';
 
 export default function HomeScreen() {
+  const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    getTrendingMovies();
+  });
+
+  const getTrendingMovies = async () => {
+    const data = await fetchTrendingMovies();
+
+    if (data && data.results)
+      setTrending(data.results);
+  }
+
   return (
-    <View className="flex mt-20">
-      <Text>Home</Text>
+    <View className="flex-1 bg-blue-400">
+
+      <ScrollView>
+        { trending.length > 0 && <TrendingMovies data={trending} />}
+      </ScrollView>
     </View>
   )
 }
